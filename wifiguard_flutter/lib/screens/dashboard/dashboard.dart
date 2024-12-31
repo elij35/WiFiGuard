c54ad3d98cf77af40b407186071d933dfd93d0a3
Eli Bowen <elijbowen@gmail.com>
2024-12-31 14:28:55 +0000

    Removed hardcoded Android wifi SSID

--- wifiguard_flutter/lib/screens/dashboard/dashboard.dart
+++ wifiguard_flutter/lib/screens/dashboard/dashboard.dart
@@ -1,14 +1,43 @@
 import 'package:WiFiGuard/screens/connected_devices/connected_devices.dart';
+import 'package:WiFiGuard/services/connected_devices_service.dart';
 import 'package:WiFiGuard/screens/help_and_guidance/help_and_info.dart';
 import 'package:WiFiGuard/screens/network_info/network_info.dart';
 import 'package:WiFiGuard/screens/settings/settings.dart';
 import 'package:WiFiGuard/widgets/tile_builder.dart';
 import 'package:flutter/material.dart';
 
-class DashboardScreen extends StatelessWidget {
+class DashboardScreen extends StatefulWidget {
   final ValueNotifier<ThemeMode> themeModeNotifier;
 
-  const DashboardScreen({super.key, required this.themeModeNotifier});
+  DashboardScreen({super.key, required this.themeModeNotifier});
+
+  @override
+  State<DashboardScreen> createState() => _DashboardScreenState();
+}
+
+class _DashboardScreenState extends State<DashboardScreen> {
+  final ConnectedDevicesService _connectedDevicesService =
+  ConnectedDevicesService();
+
+  String _wifiName = 'Unknown';
+
+  @override
+  void initState() {
+    super.initState();
+    _loadNetworkData();
+  }
+
+  Future<void> _loadNetworkData() async {
+    try {
+      // Fetch Wi-Fi name
+      final wifiName = await _connectedDevicesService.getWifiName();
+      setState(() {
+        _wifiName = wifiName;
+      });
+    } catch (e) {
+      print("Error loading network data: $e");
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,7 +52,7 @@ class DashboardScreen extends StatelessWidget {
                 context,
                 MaterialPageRoute(
                   builder: (context) =>
-                      SettingsScreen(themeModeNotifier: themeModeNotifier),
+                      SettingsScreen(themeModeNotifier: widget.themeModeNotifier),
                 ),
               );
             },
@@ -39,8 +68,8 @@ class DashboardScreen extends StatelessWidget {
               color: Color(0xff00177c),
               child: ListTile(
                 leading: const Icon(Icons.wifi, color: Colors.white),
-                title: const Text(
-                  'Android Wifi',
+                title: Text(
+                  "SSID: $_wifiName",
                   style: TextStyle(color: Colors.white),
                 ),
                 subtitle: const Text(
