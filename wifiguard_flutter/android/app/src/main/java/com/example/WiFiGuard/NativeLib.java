4657b15485195fd61a59d60ec4eb3676211dca51
Eli Bowen <41732404+elij35@users.noreply.github.com>
2025-02-12 11:00:01 +0000

    Removing redundant files

--- wifiguard_flutter/android/app/src/main/java/com/example/WiFiGuard/NativeLib.java
+++
@@ -1,9 +0,0 @@
-package com.example.WiFiGuard;
-
-public class NativeLib {
-    static {
-        System.loadLibrary("native-lib"); // Matches the library name in CMake
-    }
-
-    public static native String getMacAddress(String ip);
-}
