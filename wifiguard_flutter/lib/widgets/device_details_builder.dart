2e5d9e720fabd20b9d4f35525653cc2ffd788007
Eli Bowen <41732404+elij35@users.noreply.github.com>
2025-03-26 17:05:45 +0000

    Created Widget for device details screen

---
+++ wifiguard_flutter/lib/widgets/device_details_builder.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class DeviceDetailsBuilder extends StatelessWidget {
+  final IconData icon;
+  final String title;
+  final String value;
+  final String description;
+
+  const DeviceDetailsBuilder({
+    Key? key,
+    required this.icon,
+    required this.title,
+    required this.value,
+    required this.description,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 3,
+      margin: const EdgeInsets.symmetric(vertical: 8),
+      child: ListTile(
+        leading: Icon(icon, color: Colors.blue),
+        title: Text(title, style: const TextStyle(fontWeight: FontWeight.bold)),
+        subtitle: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(value, style: const TextStyle(fontSize: 16)),
+            const SizedBox(height: 4),
+            Text(description, style: const TextStyle(fontSize: 12)),
+          ],
+        ),
+      ),
+    );
+  }
+}
